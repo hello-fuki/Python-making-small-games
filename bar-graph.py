be82cee9009a75590b66a9b5fe0455def900e649
hello-fuki <bloomxfu@gmail.com>
2024-01-13 10:32:08 +0900

    [add] グラフ表示

--- bar-graph.py
+++ bar-graph.py
@@ -8,6 +8,13 @@ canvas = tkinter.Canvas(root,width=640,height=480)
 canvas.create_rectangle(40, 40, 600, 440, fill="gray78")
 canvas.pack()
 
+# グラフ用変数
+start_x = 60
+start_y = 60
+width_px = 5
+height_px = 32
+distance_px = 4
+
 # リスト
 list = [70, 15, 66, 21, 19, 97, 33, 44, 30, 2]
 disp = ""
@@ -19,8 +26,12 @@ for k in range(len(list) - 1, 0, -1):
             temp = list[j]
             list[j] = list[j+1]
             list[j+1] = temp
+        x = start_x
+        y = start_y
         for i in list:
-            disp = disp + str(i) + " "
-        print(disp)
-        disp = ""
+            canvas.create_rectangle(x, y, x + i * width_px, y + height_px, fill = "blue", outline="blue")
+        y = y + height_px + distance_px
+        #     disp = disp + str(i) + " "
+        # print(disp)
+        # disp = ""
 root.mainloop()
